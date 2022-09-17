04992e16a3500b25e07c3c736feafdeec346b591
emanuel <57027601+theeemanuel@users.noreply.github.com>
2022-09-18 01:00:07 +0530

    Add files via upload

---
+++ Ising Model/ising.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import colors
+
+T = 1.5
+
+N = 10000
+
+up = 1
+down = -1
+field = [up, down]
+colormap = colors.ListedColormap(["white","black"])
+
+s = np.random.choice(field, N*N).reshape(N, N)
+
+def update(data):
+    global s, T
+    newS = s.copy()
+
+    for i in range (N):
+        for j in range(N):
+            sorroundSpin = (newS[i, (j-1)%N] + newS[i, (j+1)%N] + newS[(i-1)%N, j] + newS[(i+1)%N, j])
+            E = -2*newS[i, j]*sorroundSpin
+
+            if E>0:
+                newS[i, j] *= -1
+            else:
+                r = np.random.rand()
+                #print(r)
+                if r < np.exp((2*E)/T):
+                    newS[i, j] *= -1
+    mat.set_data(newS)
+    s = newS
+    print("Simulating Generation ",data)
+    return [mat]
+
+fig, ax = plt.subplots()
+mat = plt.imshow(s, cmap=colormap)
+ani = animation.FuncAnimation(fig, update, interval=50, save_count=200)
+#ani.save('ising3.gif', writer='imagemagick', fps=60)
+plt.show()
+
+    
